05abd9605370ea85bef585c7493cb7b3aacae509
Chigwi <allie.velandia@outlook.com>
2026-09-22 15:21:40 -0500

    added spring base template

---
+++ Rmanager/src/test/java/co/edu/poli/Rmanager/RmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.poli.Rmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
