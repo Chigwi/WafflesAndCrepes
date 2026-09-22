a4810f6849b0414fbf3eb5829aeea5f735e54313
salod <salod@salito.mshome.net>
2026-09-22 15:46:16 -0500

    no se que es amiguis solo pulleen

--- Rmanager/src/main/java/co/edu/poli/Rmanager/RmanagerApplication.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/RmanagerApplication.java
@@ -3,6 +3,8 @@ package co.edu.poli.Rmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Data;
+
 @SpringBootApplication
 public class RmanagerApplication {
 
