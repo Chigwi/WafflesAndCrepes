798c8ac28cfa059f0b66f5d02e6c8dc7ba4b0b81
salod <salod@salito>
2026-09-24 17:35:45 -0500

    edicion de main

--- Rmanager/src/main/java/co/edu/poli/Rmanager/Main.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/Main.java
@@ -8,6 +8,7 @@ import co.edu.poli.Rmanager.menuService.model.entity.Categoria;
 import co.edu.poli.Rmanager.menuService.model.entity.Detalle_producto;
 import co.edu.poli.Rmanager.menuService.model.entity.Producto;
 import co.edu.poli.Rmanager.menuService.service.ProductosService;
+import co.edu.poli.Rmanager.ordenService.model.Entity.Detalle_pedido;
 import co.edu.poli.Rmanager.usuarioService.service.EmpleadoService;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -53,14 +54,47 @@ public class Main {
 		
 		Producto p4 = new Producto((long) 3, "Salchipapa", "papas y salchichas", (double) 15.0, comidaRapida, true);
 		
-		System.out.println(m.productoService.getProductoById((long)3));
+		System.out.println(m.productoService.getProductoById((long)3).toString());
 		
+	
+		
+		
+		//pruebas detalle_producto
+		
+		Detalle_pedido sp = new Detalle_pedido();
+		
+		Detalle_producto pd1 = new Detalle_producto((long)1,p1,"sin cebolla",sp);
+		
+		Detalle_producto pd2 = new Detalle_producto((long)2,p2,"con adicion de queso",sp);
+		
+		Detalle_producto pd3 = new Detalle_producto((long)3,p3,"con adicion de papas a la francesa",sp);
+		
+		m.productoService.createDetalleProducto(pd1);
+		m.productoService.createDetalleProducto(pd2);
+		m.productoService.createDetalleProducto(pd3);
+		
+		System.out.println(m.productoService.getAllDetalleProducto().toString());
+		
+		Detalle_producto pd4 = new Detalle_producto((long)1, p1, "sin cebolla y con limonada",sp);
+		
+		System.out.println(m.productoService.getDetalleProductonById((long)1).toString());
+		
+		
+		//deletes
+		
+		//producto
 		m.productoService.deleteProducto((long)1);
 		m.productoService.deleteProducto((long)2);
 		m.productoService.deleteProducto((long)3);
 		
+		//detalle prpodcuto
+		m.productoService.deleteDetalle_producto((long)1);
+		m.productoService.deleteDetalle_producto((long)2);
+		m.productoService.deleteDetalle_producto((long)3);
 		
-		//pruebas detalle_producto
+		
+		
+		
 	}
 	
 
