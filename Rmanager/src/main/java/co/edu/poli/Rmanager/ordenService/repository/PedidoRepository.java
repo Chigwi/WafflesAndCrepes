cff72813233995dc2eff46867ca69304afa43aef
Chigwi <allie.velandia@outlook.com>
2026-09-24 16:31:22 -0500

    crud basico pedidoService

--- Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/repository/PedidoRepository.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/repository/PedidoRepository.java
@@ -1,5 +1,12 @@
 package co.edu.poli.Rmanager.ordenService.repository;
 
-public interface PedidoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.poli.Rmanager.ordenService.model.Entity.Pedido;
+
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
 }
--- Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/service/PedidoService.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/service/PedidoService.java
@@ -1,5 +1,58 @@
 package co.edu.poli.Rmanager.ordenService.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.edu.poli.Rmanager.ordenService.model.Entity.Pedido;
+import co.edu.poli.Rmanager.ordenService.repository.PedidoRepository;
+
 public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	//create pedido
+	
+	public Pedido createPedido(Pedido pedido) {
+		
+		return pedidoRepository.save(pedido);
+		
+	}
+	
+	//read all pedidos
+	
+	public List<Pedido> getAllPedidos() {
+		
+		return pedidoRepository.findAll();
+		
+	}
+	
+	//read pedido by id
+	
+	public Optional<Pedido> getPedidoById(Long id){
+	
+		return pedidoRepository.findById(id);
+		
+	}
+	
+	//delete pedido
+	
+	public boolean deletePedido(Long id) {
+	    if (pedidoRepository.existsById(id)) {
+	        pedidoRepository.deleteById(id);
+	        return true;
+	    }
+	    return false;  // Returns false if ID not found
+	}
+	
+	//update pedido
+	
+	public Pedido updatePedido(Pedido newPedido) {
+		
+		return pedidoRepository.save(newPedido);
+		
+	}
 
 }
