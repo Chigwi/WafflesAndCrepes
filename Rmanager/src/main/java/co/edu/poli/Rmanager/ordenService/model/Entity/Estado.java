39923a268e1c25e7fcff2bcbc53b51e97bcd4560
Chigwi <allie.velandia@outlook.com>
2026-09-24 15:10:58 -0500

    pojo estado

--- Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/model/Entity/Estado.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/ordenService/model/Entity/Estado.java
@@ -1,5 +1,19 @@
 package co.edu.poli.Rmanager.ordenService.model.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Estado")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Estado {
 
+	
+	
 }
