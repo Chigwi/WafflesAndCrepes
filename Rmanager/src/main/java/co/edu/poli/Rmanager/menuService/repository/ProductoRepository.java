9c3276ebe4164fafffa1f0a3085e8ea4bd705786
salod <salod@salito>
2026-09-24 16:06:24 -0500

    crecion de paquete service y repository menuService

---
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/repository/ProductoRepository.java
@@ -0,0 +1,5 @@
+package co.edu.poli.Rmanager.menuService.repository;
+
+public interface ProductoRepository {
+
+}
---
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/service/ProductosService.java
@@ -0,0 +1,5 @@
+package co.edu.poli.Rmanager.menuService.service;
+
+public class ProductosService {
+
+}
