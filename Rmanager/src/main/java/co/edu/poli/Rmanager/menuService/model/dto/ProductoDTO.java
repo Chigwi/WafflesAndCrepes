a224bcfb32c559d4065b3eae8cbf417f01f7164e
salod <salod@salito>
2026-09-24 15:06:09 -0500

    Creacion de paquete entity y dto de servicio menu

---
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/model/dto/ProductoDTO.java
@@ -0,0 +1,5 @@
+package co.edu.poli.Rmanager.menuService.model.dto;
+
+public class ProductoDTO {
+
+}
---
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/model/entity/DetalleProducto.java
@@ -0,0 +1,5 @@
+package co.edu.poli.Rmanager.menuService.model.entity;
+
+public class DetalleProducto {
+
+}
