58ec4f10633cf1aa45cd1c33256cfaef9c98ad19
Chigwi <allie.velandia@outlook.com>
2026-09-24 15:44:35 -0500

    correción pojos empleado

--- Rmanager/src/main/java/co/edu/poli/Rmanager/usuarioService/model/entity/Empleado.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/usuarioService/model/entity/Empleado.java
@@ -1,6 +1,17 @@
 package co.edu.poli.Rmanager.usuarioService.model.entity;
 
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import co.edu.poli.Rmanager.ordenService.model.Entity.Historial_estado;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +23,31 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Empleado {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_empleado;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido")
+	private String apellido;
+	
+	@Column(name = "cedula")
+	private String cedula;
+	
+	@Column(name = "correo")
+	private String correo;
+	
+	@ManyToOne
+	@JoinColumn(name = "rol")
+	private Long rol;
+	
+    @OneToMany(mappedBy = "empleado_que_relevo", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Historial_estado> cambiosEstado;
+	
 }
--- Rmanager/src/main/java/co/edu/poli/Rmanager/usuarioService/model/entity/Rol.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/usuarioService/model/entity/Rol.java
@@ -1,6 +1,12 @@
 package co.edu.poli.Rmanager.usuarioService.model.entity;
 
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,4 +20,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Rol {
 
+	
+	@Id
+	private Long id_rol;
+
+	@Column
+	private String nombre;
+	
+    @OneToMany(mappedBy = "rol", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Empleado> empleados;
+    
 }
