9fbca1f0d6194b8676200a8bcbed542dd35e5a20
salod <salod@salito>
2026-09-24 15:11:41 -0500

    pollo clase Categoria

--- Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/model/entity/Categoria.java
+++ Rmanager/src/main/java/co/edu/poli/Rmanager/menuService/model/entity/Categoria.java
@@ -1,5 +1,20 @@
 package co.edu.poli.Rmanager.menuService.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table (name="Categoria")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+	
+	
 
 }
